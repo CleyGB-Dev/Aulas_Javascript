324e73ec51584294a8969ece9f6a36f187e26cfb
CleyGB-Dev <cleytongabrielbarbosa@gmail.com>
2025-08-19 19:50:38 -0300

    prosseguimento das aulas

---
+++ aula05/ex.js
@@ -0,0 +1,12 @@
+var nome = " ", sobrenome = " ";
+
+console.prompt("Digite seu nome: ", function(input) {
+    nome = input;
+});
+
+console.prompt("Digite seu sobrenome: ", function(input) {
+    sobrenome = input;
+});
+
+console.log(nome);
+console.log(sobrenome);
