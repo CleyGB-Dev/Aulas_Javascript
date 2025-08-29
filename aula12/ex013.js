76e56a638f9e5a529b1da4e6e46f9a0aedfc5353
CleyGB-Dev <cleytongabrielbarbosa@gmail.com>
2025-08-28 23:31:19 -0300

    Create ex013.js

---
+++ aula12/ex013.js
@@ -0,0 +1,28 @@
+var idade = 25
+console.log(`Você tem ${idade} anos.`)
+switch (true) {
+    case idade < 0:
+        console.log('Você não existe ainda')
+        break
+    case idade <= 3:
+        console.log('Você é um Bebê')
+        break
+    case idade <= 12:
+        console.log('Você é uma Criança')
+        break
+    case idade < 18:
+        console.log('Você é um Adolescente')
+        break
+    case idade >= 18 && idade < 60:
+        console.log('Você é um Adulto')
+        break
+    case idade >= 60 && idade <= 100:
+        console.log('Você é um Idoso')
+        break
+    case idade > 100:
+        console.log('Você é um Centenário')
+        break
+
+    default:
+        console.log('Idade inválida')
+}
